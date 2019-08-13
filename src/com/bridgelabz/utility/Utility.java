a4e247238de0d11336d45bdec2bfc70e9f8b5a55
VenomRishi <mhatrerishikesh@gmail.com>
2019-08-13 21:30:41 +0530

    structure of project is ready

---
+++ src/com/bridgelabz/utility/Utility.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Purpose: This is utility file which contains logic of all programs.
+ *
+ *  @author  Rishikesh Mhatre
+ *  @version 1.0
+ *  @since   13-08-2019
+ *  @updated 13-08-2019
+ *
+ ******************************************************************************/
+
+package com.bridgelabz.utility;
+
+public class Utility {
+
+}
